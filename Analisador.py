48501be2b5f37e76d1ea36410fe8e781cc300df1
Airam <139919189+viniairam7@users.noreply.github.com>
2023-09-22 07:32:02 -0300

    Create Analisador.py

---
+++ Analisador.py
@@ -0,0 +1,25 @@
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+for p in range (1, 5):
+    print('{}° Pessoa'.format(p))
+    nome = str(input('Nome:  ')).strip()
+    idade = int(input('Idade:  '))
+    sexo = str(input('Sexo [M/F]:  ')).strip()
+    print('//' * 10)
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
+mediaidade = somaidade / 4
+print('A soma de idade é de {} anos.'.format(mediaidade))
+print('O {} é o mais velho e tem {} anos.'.format(nomevelho, maioridadehomem))
+print('Ao todo são {} mulheres com menos de 20 anos'.format(totmulher20))
+
